3b07305ed9861fbee8157bbf6ec632829b23ce3a
abdul rahman <amanasmuei@gmail.com>
2026-04-30 13:23:15 +0800

    chore: ignore scripts/ in eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   {
-    ignores: ["dist/**", "node_modules/**", "coverage/**", "*.config.js", "*.config.ts"],
+    ignores: ["dist/**", "node_modules/**", "coverage/**", "*.config.js", "*.config.ts", "scripts/**"],
   },
   ...tseslint.configs.strictTypeChecked,
   ...tseslint.configs.stylisticTypeChecked,
